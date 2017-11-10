548f2e3671e732be13c5cca975f787d2d7d2f1bf
Sherry <sherryptk@gmail.com>
2017-11-10 20:04:51 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,20 +12,22 @@ def fourth_challenge
           :attitude => "worried"
         }
          {:name => "Mercutio", :age => "18", :attitude => "hot-headed"
-          
+
         }
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {:name => "Steven", :age => "30", :attitude => "confused"}
+        {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
 }
 
-  
+
 
 end
